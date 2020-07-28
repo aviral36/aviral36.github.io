f13b980213e7e186d122f58b24275af56fc8820e
Aviral Sharma <aviral-s@outlook.com>
2020-07-28 12:55:56 +0530

    link javacript functions to buttons.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,24 @@
-let num1;
-let num2;
-function multiply(num1,num2) {
-    let result = num1 * num2;
-    const ans = document.querySelector('h3');
-    ans.textContent = result;
-}
-multiply(4,2);
+var mode;
+var category;
+var difficulty;
 
-document.querySelector('body').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+function startGame() { 
+    var player_mode = document.getElementsByName('mode'); 
+    for(i = 0; i < player_mode.length; i++) { 
+        if(player_mode[i].checked)
+        mode = player_mode[i].value; 
+    }
+    var player_cat = document.getElementsByName('category'); 
+    for(i = 0; i < player_cat.length; i++) { 
+        if(player_cat[i].checked)
+        category = player_cat[i].value; 
+    }
+    var player_diff = document.getElementsByName('difficulty'); 
+    for(i = 0; i < player_diff.length; i++) { 
+        if(player_diff[i].checked)
+        difficulty = player_diff[i].value; 
+    }
+    document.body.innerHTML = "";
+    document.getElementById("result").innerHTML
+                        = "Mode: "+mode + " Category: "+category+" Difficulty: "+difficulty;
+} 
